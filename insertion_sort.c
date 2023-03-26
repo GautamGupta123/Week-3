597453ff58acb1e091aa6b1dfa3c238116e72186
Gautam Gupta <112850367+GautamGupta123@users.noreply.github.com>
2023-03-26 14:32:58 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int arr[50],i,n,j,temp;
+	int arr[50],i,n,j,temp,comparisions=0,shifts=0;
 	printf("Enter the range:");
 	scanf("%d",&n);
 	for(i=0;i<=n-1;i++)
@@ -13,14 +13,19 @@ int main()
 	  j=i-1;
 	  while(key<arr[j] && j>=0)
 	  {
+		comparisions++;
 	  	arr[j+1]=arr[j];
+		shifts++;
 	  	j--;
 	  }
-	  arr[j+1]=key;   
+	  arr[j+1]=key;  
+	  shifts++;
 	}	
 	printf("Sorted array is \n");
 	for(i=0;i<=n-1;i++){
 		printf("%d ",arr[i]);
 	}
+	printf("\nComparisions=%d\n",comparisions);
+	printf("Shifts=%d",shifts);
 	return 0;
 }
